e11a61a339e00979ba03803e8093499efccab0f5
tiendung311 <109157322+tiendung311@users.noreply.github.com>
2024-08-15 14:28:06 +0700

    init

---
+++ src/main/java/com/example/expenses_manage/ExpensesManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenses_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expenses_manage/ExpensesManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenses_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
